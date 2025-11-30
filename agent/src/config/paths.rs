8feba635e0e6177bffbafec105866fcb47802c33
Daniil Oberlev <denioberlev@gmail.com>
2025-11-30 08:06:03 +0300

    feat(config): simplified functions

--- agent/src/config/paths.rs
+++ agent/src/config/paths.rs
@@ -1,16 +1,16 @@
 use std::path::PathBuf;
 
-pub fn bpf_object() -> PathBuf {
+fn project_root() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .parent()
         .unwrap()
-        .join("bpf")
-        .join("prog.bpf.o")
+        .to_path_buf()
+}
+
+pub fn bpf_object() -> PathBuf {
+    project_root().join("bpf/prog.bpf.o")
 }
 
 pub fn malware_domains() -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .parent()
-        .unwrap()
-        .join("malware_domains.txt")
+    project_root().join("malware_domains.txt")
 }
